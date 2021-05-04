3c86c0b95a83312b421ca4017050d0bf972f9f44
unknown <dany.roman.phd@gmail.com>
2021-05-04 16:56:55 -0500

    Added comments

--- election-summary.py
+++ election-summary.py
@@ -1,27 +1,25 @@
+# import os and csv modules
 import os
 import csv
+
+# create a reference to the csv file location
 csvpath = os.path.join('election-summary','Test Data','election_data.csv')
 
+#create empty lists for total votes and candidate votes
 votes = []
 khanvotes = []
 correyvotes = []
 livotes = []
 otooleyvotes = []
-# candidates = []
+
+# open the csv file to collect data
 
 with open(csvpath) as csvfile:
 
-    # CSV reader specifies delimiter and variable that holds contents
+# create read csv file and separated data using comma 
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-    print(csvreader)
-
-#     prev_row = 0
-#     delta = 0
+# create a loop to collect vote data based on candidate
 
     for row in csvreader:
         votes.append(row[0])
@@ -34,41 +32,45 @@ with open(csvpath) as csvfile:
         elif row[2] == "O'Tooley":
             otooleyvotes.append(row[0])    
 
-    nvotes = len(votes)
-    khan = len(khanvotes)
-    correy = len(correyvotes)
-    li = len(livotes)
-    otooley = len(otooleyvotes)
-    perkhan = round((khan/nvotes * 100), 1)
-    percorrey = round((correy/nvotes * 100), 1)
-    perli = round((li/nvotes * 100), 1)
-    perotooley = round((otooley/nvotes * 100), 1)
-    
-    if (khan > correy) and (khan > li) and (khan > otooley):
-        winner = "Khan"
-    elif (correy > khan) and (correy > li) and (correy > otooley):
-        winner = "Correy"
-    elif (li > khan) and (li > correy) and (li > otooley):
-        winner = "Li"
-    elif (otooley > khan) and (otooley > correy) and (otooley > li):
-        winner = "OTooley"
+# calculate the total number of votes and votes for each candidate
+nvotes = len(votes)
+khan = len(khanvotes)
+correy = len(correyvotes)
+li = len(livotes)
+otooley = len(otooleyvotes)
 
-line1 = print(f"------------------------------------------")
-line2 = print(f"Election Results")
-line3 = print(f"------------------------------------------")
-line4 = print(f'Total votes cast: {nvotes}')
-line5 = print(f"------------------------------------------")
-line6 = print(f'Khan: {khan} ({perkhan}%)')
-line7 = print(f'Correy: {correy} ({percorrey}%)')
-line8 = print(f'Li: {li} ({perli}%) ')
-line9 = print(f'OTooley: {otooley} ({perotooley}%)')
-line10 = print(f"------------------------------------------")
-line11 = print(f'Winner: {winner}')
-line12 = print(f"------------------------------------------")
+# calculate the percentage of votes per candidate
+perkhan = round((khan/nvotes * 100), 1)
+percorrey = round((correy/nvotes * 100), 1)
+perli = round((li/nvotes * 100), 1)
+perotooley = round((otooley/nvotes * 100), 1)
 
-summary = [line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12]
+# identify the winner using a series of conditional statements
+if (khan > correy) and (khan > li) and (khan > otooley):
+    winner = "Khan"
+elif (correy > khan) and (correy > li) and (correy > otooley):
+    winner = "Correy"
+elif (li > khan) and (li > correy) and (li > otooley):
+    winner = "Li"
+elif (otooley > khan) and (otooley > correy) and (otooley > li):
+    winner = "OTooley"
 
-with open("election-summary","ElectionSummary.txt", "w+") as output:
+# display results on terminal
+print(f"------------------------------------------")
+print(f"Election Results")
+print(f"------------------------------------------")
+print(f'Total votes cast: {nvotes}')
+print(f"------------------------------------------")
+print(f'Khan: {khan} ({perkhan}%)')
+print(f'Correy: {correy} ({percorrey}%)')
+print(f'Li: {li} ({perli}%)')
+print(f'OTooley: {otooley} ({perotooley}%)')
+print(f"------------------------------------------")
+print(f'Winner: {winner}')
+print(f"------------------------------------------")
+
+# create an election summary file and write the results into it. Close at the end
+with open("ElectionSummary.txt", "w+") as output:
 
     print(f"------------------------------------------", file=output)
     print(f"Election Results", file=output)
